9a2382af1eb4ce8b509fae10bfe471c6cad932bd
Akshay Jagtap <ajagtap2804@gmail.com>
2026-04-07 19:18:57 +0530

    Updated schema with primary keys

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,19 +1,35 @@
+-- RAW TABLES
 CREATE TABLE customers_raw (
-    customer_id INT,
+    customer_id INT PRIMARY KEY,
     name TEXT,
     city TEXT
 );
 
 CREATE TABLE products_raw (
-    product_id INT,
+    product_id INT PRIMARY KEY,
     name TEXT,
     price FLOAT
 );
 
 CREATE TABLE orders_raw (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    quantity INT,
+    order_date DATE
+);
+
+-- CLEAN TABLE
+CREATE TABLE orders_clean (
     order_id INT,
     customer_id INT,
     product_id INT,
     quantity INT,
     order_date DATE
+);
+
+-- ANALYTICS TABLE
+CREATE TABLE customer_ltv (
+    customer_id INT,
+    total_spent FLOAT
 );
